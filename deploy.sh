6dec6f515028dcfca6ba285f0d4b04e485331e76
xushuai <dyxushuai@gmail.com>
2017-08-25 16:41:57 +0800

    rebuilding site Fri Aug 25 16:41:57 CST 2017

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,4 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-git subtree push --prefix=public git@github.com:JohnSmithX/yyofme.git gh-pages
+git subtree push --prefix=public git@github.com:xusss/yyofme.git gh-pages
